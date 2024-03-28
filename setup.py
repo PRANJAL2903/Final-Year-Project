d7d48b1cce8e0683bb59bd2e178696929f302a77
PRANJAL2903 <84214223+PRANJAL2903@users.noreply.github.com>
2024-03-28 22:58:34 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="pranjal",
+    author_email="pranjal92chaudhari@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
